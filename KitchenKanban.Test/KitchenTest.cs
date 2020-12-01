067bcc724b53719bc0377abbe4654f08ab633fb6
sujeeshs <sujeesh@smartroute4u.com>
2020-12-01 09:07:21 +0530

    Unit test

--- KitchenKanban.Test/KitchenTest.cs
+++ KitchenKanban.Test/KitchenTest.cs
@@ -2,6 +2,7 @@ using KitchenKanban.BusinessServices;
 using KitchenKanban.BusinessServices.Interfaces;
 using KitchenKanban.DataServices.Context;
 using KitchenKanban.DataServices.UserInfo;
+using KitchenKanban.ViewModels;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
@@ -14,20 +15,18 @@ namespace KitchenKanban.Test
         public void CreateKitchen()
         {
             var serviceCollection = new ServiceCollection();
-
             serviceCollection.AddDbContext<KitchenKanbanDB>(options => options.UseInMemoryDatabase("KitchenKanbanDB"), ServiceLifetime.Transient);
-
             ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
 
-            IUserInfo userinfo = new UserInfo()
+            IUserInfo userInfo = new UserInfo()
             {
                 UserId = "95632324-a9f8-44ba-9b3d-4c90dd5d9650",
                 UserType = Models.Enums.UserEnum.UserType.Administrator
             };
 
-            IKitchenService kitchenService = new KitchenService(serviceProvider, userinfo);
+            IKitchenService kitchenService = new KitchenService(serviceProvider, userInfo);
 
-            var kitchens = kitchenService.Create(new ViewModels.KitchenViewModel()
+            var kitchen = kitchenService.Create(new KitchenViewModel()
             {
                 CounterNumber = "My Counter 1"
             });
