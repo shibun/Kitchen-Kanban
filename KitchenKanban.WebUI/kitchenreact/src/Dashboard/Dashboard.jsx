fcbe5dfa0c0a2115573e27288957382ac38619fb
jayanth@smartroute4u.com <jayanth@smartroute4u.com>
2020-12-21 17:24:49 +0530

    dashboard update

--- KitchenKanban.WebUI/kitchenreact/src/Dashboard/Dashboard.jsx
+++ KitchenKanban.WebUI/kitchenreact/src/Dashboard/Dashboard.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { dashboardActions } from '../_actions';
+import { dashboardService } from '../_services';
+
 import { Header } from '../_components/Header';
 import{NewOrder} from '../_components/NewOrder';
+import moment from 'moment';
 
 class DashboardPage extends React.Component {
  constructor(props) {
         super(props); 
         this.state={
+            currentdate:moment(new Date()).format("DD/MM/YYYY hh:mm a"),
             showform:false,          
             neworders: [],
             beingpreparedorders: [],
@@ -46,7 +50,6 @@ class DashboardPage extends React.Component {
           const packingorders = nextProps.kanbanorders.items.filter((item) => item.bucketName === "Packing" )[0];
           const readyorders = nextProps.kanbanorders.items.filter((item) => item.bucketName ===  "Ready To Be Delivered" ||item.bucketName === "Takeaway"  )[0];
           console.log('neworders',neworders);
-
           this.setState({
             neworders: neworders,
             beingpreparedorders: beingpreparedorders,
@@ -57,36 +60,61 @@ class DashboardPage extends React.Component {
           })
       }
     }
+
+    // if (nextProps.orderdetails !== this.props.orderdetails) {
+
+    //   this.setState({
+    //     neworders: {
+    //        ...this.state.neworders,
+    //        ['idea']: 'something'
+    //     }
+    //   });
+
+    //    console.log('order details set',this.state.neworders);
+    // }
   }
 
   getordertime(orderdate){
-        var initial = new Date(orderdate);
+      var initial = new Date(orderdate);
       var final = new Date();  
       var difference = (Math.floor(final.getTime()-initial.getTime())/36e6);  
-    return difference;
+    return difference.toFixed(2);
+    //return moment(String(difference)).format('hh:mm a')
    }
 
   toggleOrderDetails(order, index, dividentifier, bucketname) {   
-    console.log('order in toggle',order); 
-    // if(order.orderStatus==6){
-    //   console.log('if block executed in sidetoggle');
-    //   return
-    // }
-    // else{
-    //   console.log('else block executed in toggle');
-    //    $("#orderdetailsdiv" + dividentifier + index).slideToggle();
-    //    this.showorderdetails = !this.showorderdetails;
-    //    this.getOrderDetails(order);
-    //    this.getFilteredStatus(bucketname, order);
+    if(order.orderStatus==6){
+      return
+    }
+    else{
+       $("#orderdetailsdiv" + dividentifier + index).slideToggle();
+       this.showorderdetails = !this.showorderdetails;
+       this.getOrderDetails(order);
+       //this.getFilteredStatus(bucketname, order);
        
-    //   }
+      }
   }
-   
+  getOrderDetails(data) {
+    this.isAddOrder = false;
+    dashboardService.getOrderDetails(data.orderId)
+      .then((response) => {
+        var newitems= this.state.neworders.map(item =>
+          item.orderId === response.data.order.orderId
+            ? { ...item, orderLines: response.data.orderLines }
+            : item
+        )
+
+        this.setState({
+          neworders:newitems
+        })
+      })
+      .catch((err) => {
+        (this.errormsg = err.messge), console.log(err.message);
+      });
+  }
     render() {
-      const { kanbanorders } = this.props;
-      const {neworders,showform}=this.state;
-        console.log('neworders',neworders);
-     
+      const { kanbanorders,orderdetails } = this.props;
+      const {neworders,showform,currentdate}=this.state;
         return (
           <div>
 
@@ -94,7 +122,7 @@ class DashboardPage extends React.Component {
 
     <div className="breadcrumb">
         <div>Dashboard</div>
-        <div className="current-time">Time </div>
+        <div className="current-time">{currentdate} </div>
         <div className="clearfix"></div>
       </div>
  
@@ -134,7 +162,7 @@ class DashboardPage extends React.Component {
 {/* //v-for new orders.orders       */} 
 {neworders && neworders.map((order, index) => (
               <div id="ordermaindivno" className="ticket-main" key={index}>
-                <div className="order-detail" onClick={this.toggleOrderDetails(order, index, 'no', 'neworder')}>
+                <div className="order-detail" onClick={e=>this.toggleOrderDetails(order, index, 'no', 'neworder')}>
                   <div>
                     <div className="float-left"><img src="../../src/_assets/images/order_number_icon.png"/> { order.orderNumber.split("-")[1]}</div>
                     <div className="float-right"><img src="../../src/_assets/images/time_icon.png"/>{this.getordertime(order.orderDate)}</div>                    
@@ -143,8 +171,8 @@ class DashboardPage extends React.Component {
                   
                 </div>
 {/* //v-if="order.orderLines" */}
-                <div id="'orderdetailsdivno' + index" className="order-description" 
-                >
+            {order && order.orderLines &&
+                <div id={"orderdetailsdivno"+index} className="order-description">
                   <table className="order-tbl" >
                     <tbody>
                       <tr className="order-tbl-header">
@@ -153,23 +181,24 @@ class DashboardPage extends React.Component {
                         <td className="text-center">Price</td>
                       </tr>
                       {/* v-for="(orderline, index) in order.orderLines" */}
-                      {order.orderLines &&
-                      <tr >
-                        <td>{ orderline.itemName }</td>
-                        <td className="text-center">
-                          { orderline.orderQuantity }
-                        </td>
-                        <td className="text-right">{ orderline.itemCharge * orderline.orderQuantity}</td>
-                      </tr>
-    }
+                        {order.orderLines && order.orderLines.map((orderline, index) => (
+                            <tr key={index}>
+                              <td>{ orderline.itemName }</td>
+                              <td className="text-center">
+                                { orderline.orderQuantity }
+                              </td>
+                              <td className="text-right">{ orderline.itemCharge * orderline.orderQuantity}</td>
+                            </tr>
+                        ))}
                         <tr>
                            <th>Total</th>
-                            <th className="text-center">{order.noOfItemsInOrder}</th>
+                            <th className="text-center">{order.idea}{order.noOfItemsInOrder}</th>
                             <th className="text-right">{order.orderAmount}</th>
                          </tr>
                     </tbody>
                   </table>
                 </div>
+              }
               </div>
 ))} 
             </div>
@@ -188,12 +217,13 @@ class DashboardPage extends React.Component {
 }
 
 function mapStateToProps(state) {
-  const { kanbanorders } = state;
-  return { kanbanorders };
+  const { kanbanorders,orderdetails } = state;
+  return { kanbanorders,orderdetails };
 }
 
 const actionCreators = {
 getKanboard: dashboardActions.getKanboard,
+getOrderDetails: dashboardActions.getOrderDetails,
 
 }
 const connectedDashboardPage = connect(mapStateToProps,actionCreators)(DashboardPage);
