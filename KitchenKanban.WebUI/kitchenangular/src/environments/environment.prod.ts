de45986037c40d64a2be81450d802ed83652986a
sujeeshs <sujeesh@smartroute4u.com>
2021-01-08 13:09:23 +0530

    production environment

--- KitchenKanban.WebUI/kitchenangular/src/environments/environment.prod.ts
+++ KitchenKanban.WebUI/kitchenangular/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  tokenEndpoint: 'http://localhost:64464/WebApi/token',
+  apiEndpoint: 'http://localhost:64464/WebApi/api'
 };
